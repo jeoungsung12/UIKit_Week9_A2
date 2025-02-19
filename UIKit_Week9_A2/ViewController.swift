1b4ab91b11c0365bc3fee7bba3623c250640b3f2
jeoungsung12 <jeoungsung12@naver.com>
2025-02-19 16:00:15 +0900

    Initial Commit

---
+++ UIKit_Week9_A2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit_Week9_A2
+//
+//  Created by 정성윤 on 2/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
